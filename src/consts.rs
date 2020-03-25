f983acd7ba8dc4dacdffc5d0659846b7dbe2677a
Jonathan Klimt <jonathan.klimt@rwth-aachen.de>
2020-03-25 11:58:48 +0100

    Fixed compiler warnings

--- src/consts.rs
+++ src/consts.rs
@@ -11,10 +11,10 @@ pub const BOOT_GDT_MAX: u64 = 3;
 pub const BOOT_PML4: u64 = 0x10000;
 pub const BOOT_PDPTE: u64 = 0x11000;
 pub const BOOT_PDE: u64 = 0x12000;
-pub const EFER_SCE: u64 = (1 << 0); /* System Call Extensions */
-pub const EFER_LME: u64 = (1 << 8); /* Long mode enable */
-pub const EFER_LMA: u64 = (1 << 10); /* Long mode active (read-only) */
-pub const EFER_NXE: u64 = (1 << 11); /* PTE No-Execute bit enable */
+pub const EFER_SCE: u64 = 1 << 0; /* System Call Extensions */
+pub const EFER_LME: u64 = 1 << 8; /* Long mode enable */
+pub const EFER_LMA: u64 = 1 << 10; /* Long mode active (read-only) */
+pub const EFER_NXE: u64 = 1 << 11; /* PTE No-Execute bit enable */
 pub const COM_PORT: u16 = 0x3f8;
 pub const SHUTDOWN_PORT: u16 = 0xf4;
 pub const PIC_MASTER_PORT: u16 = 0x20;
@@ -23,9 +23,9 @@ pub const PIC_SLAVE_PORT: u16 = 0xA0;
 /*
  * Intel long mode page directory/table entries
  */
-pub const X86_PDPT_P: u64 = (1u64 << 0); /* Present */
-pub const X86_PDPT_RW: u64 = (1u64 << 1); /* Writeable */
-pub const X86_PDPT_US: u64 = (1u64 << 2); /* User/supervisor */
-pub const X86_PDPT_PS: u64 = (1u64 << 7); /* Page size */
+pub const X86_PDPT_P: u64 = 1u64 << 0; /* Present */
+pub const X86_PDPT_RW: u64 = 1u64 << 1; /* Writeable */
+pub const X86_PDPT_US: u64 = 1u64 << 2; /* User/supervisor */
+pub const X86_PDPT_PS: u64 = 1u64 << 7; /* Page size */
 
 pub const GUEST_PAGE_SIZE: u64 = 0x200000; /* 2 MB pages in guest */
--- src/error.rs
+++ src/error.rs
@@ -1,4 +1,3 @@
-use std::convert::From;
 use std::{fmt, result};
 #[cfg(target_os = "macos")]
 use xhypervisor;
