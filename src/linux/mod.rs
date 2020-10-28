1bfb16c4abc090dbbc09de535024baa58718c145
Jonathan Klimt <jonathan.klimt@rwth-aachen.de>
2020-10-28 19:59:15 +0100

    Fixed formatting of previous commit

--- src/linux/mod.rs
+++ src/linux/mod.rs
@@ -4,5 +4,6 @@ pub mod vcpu;
 use kvm_ioctls::Kvm;
 
 lazy_static! {
-	static ref KVM: Kvm = { Kvm::new().expect("KVM is not installed on this system or privileges are missing") };
+	static ref KVM: Kvm =
+		{ Kvm::new().expect("KVM is not installed on this system or privileges are missing") };
 }
