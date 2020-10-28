89266fdd36639bf014bf2dc533defc9caf66a808
Jonathan Klimt <jonathan.klimt@rwth-aachen.de>
2020-10-28 19:57:02 +0100

    Added better error message for missing KVM

--- src/linux/mod.rs
+++ src/linux/mod.rs
@@ -4,5 +4,5 @@ pub mod vcpu;
 use kvm_ioctls::Kvm;
 
 lazy_static! {
-	static ref KVM: Kvm = { Kvm::new().unwrap() };
+	static ref KVM: Kvm = { Kvm::new().expect("KVM is not installed on this system or privileges are missing") };
 }
