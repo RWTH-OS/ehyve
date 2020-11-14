c1fa13e5d267cc2d15768302d776849186474fd4
Martin Kröning <mkroening@posteo.net>
2020-11-14 11:23:32 +0100

    Remove unnecessary braces

--- src/linux/mod.rs
+++ src/linux/mod.rs
@@ -5,5 +5,5 @@ use kvm_ioctls::Kvm;
 
 lazy_static! {
 	static ref KVM: Kvm =
-		{ Kvm::new().expect("KVM is not installed on this system or privileges are missing") };
+		Kvm::new().expect("KVM is not installed on this system or privileges are missing");
 }
